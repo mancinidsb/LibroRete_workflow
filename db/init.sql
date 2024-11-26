98fa5d317b8c7ad5d882a9523ed1db90f7bfac30
mancinidsb <guilhermemancinidsb@gmail.com>
2024-11-26 09:03:22 -0300

    close issue #27

--- db/init.sql
+++ db/init.sql
@@ -53,7 +53,8 @@ INSERT IGNORE INTO perfil (id, bio, interesses, id_usuario_perfil) VALUES
 INSERT IGNORE INTO lista (id, nome, descricao, id_perfil_lista) VALUES
 (435, 'livros de 2024','meus favoritos de 2024', 1),
 (546, 'desejados','minha lista de desejos :)', 1),
-(325, 'top ever!','top dos tops', 1);
+(325, 'top ever!','top dos tops', 1),
+(294, 'lista do mancini', 'lista pessoal', 2);
 
 INSERT IGNORE INTO livro (isbn, titulo, autor, genero) VALUES
 (9788581051031, 'Carrie','S. King', 'Terror'),
@@ -71,3 +72,70 @@ INSERT IGNORE INTO lista_livro (id_lista, isbn_livro) VALUES
 (325, 9786555600155),
 (325, 9788576572374);
 
+
+
+DROP PROCEDURE IF EXISTS busca_livros_por_username;
+
+DELIMITER $$
+CREATE PROCEDURE busca_livros_por_username(IN username_param VARCHAR(20))
+BEGIN
+    SELECT 
+        lista.nome AS nome_lista,
+        lista.id,
+        livro.titulo
+    FROM 
+        perfil 
+    INNER JOIN usuario ON perfil.id_usuario_perfil = usuario.id 
+    INNER JOIN lista ON lista.id_perfil_lista = perfil.id_usuario_perfil
+    INNER JOIN lista_livro ON lista.id = lista_livro.id_lista
+    INNER JOIN livro ON livro.isbn = lista_livro.isbn_livro
+    WHERE usuario.username = username_param;
+END$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS busca_qtd_livros_de_uma_lista_por_username;
+
+DELIMITER $$
+
+CREATE PROCEDURE busca_qtd_livros_de_uma_lista_por_username(IN lista_nome VARCHAR(150), IN username_param VARCHAR(20))
+BEGIN
+    SELECT 
+        COUNT(*) AS total
+    FROM 
+        perfil 
+    INNER JOIN usuario ON perfil.id_usuario_perfil = usuario.id 
+    INNER JOIN lista ON lista.id_perfil_lista = perfil.id_usuario_perfil
+    INNER JOIN lista_livro ON lista.id = lista_livro.id_lista
+    INNER JOIN livro ON livro.isbn = lista_livro.isbn_livro
+    WHERE 
+		usuario.username = username_param AND lista.nome=lista_nome;
+END$$
+
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS busca_listas_de_um_usuario;
+DELIMITER $$
+
+CREATE PROCEDURE busca_listas_de_um_usuario(IN username_param VARCHAR(20))
+BEGIN
+    SELECT 
+    lista.nome AS nome_lista
+    FROM perfil
+    INNER JOIN lista ON perfil.id=lista.id_perfil_lista
+    INNER JOIN usuario ON perfil.id=usuario.id
+    WHERE usuario.username=username_param;
+
+END$$
+
+DELIMITER ;
+
+
+
+-- Busca TODOS os livros que o usuário cadastrou em diferentes listas
+CALL busca_livros_por_username('@eduarda');
+
+-- Busca TODAS as listas de um usuário
+CALL busca_listas_de_um_usuario('@eduarda');
+
+-- Busca a quantidade de livros de uma lista x em um perfl y
+CALL busca_qtd_livros_de_uma_lista_por_username('top ever!','@eduarda');

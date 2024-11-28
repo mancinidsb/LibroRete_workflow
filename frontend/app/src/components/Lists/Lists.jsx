ff1760b1a1bccdda0f644cf13f6bf991ddb5342a
Maria Eduarda <dudaa.mendes1502@gmail.com>
2024-11-28 07:25:29 -0300

    close issue#7

--- frontend/app/src/components/Lists/Lists.jsx
+++ frontend/app/src/components/Lists/Lists.jsx
@@ -1,10 +1,33 @@
 import './Lists.css'
 import React from 'react'
+import axios from 'axios'
 
+import { useEffect, useState } from 'react'
 import { GoPlusCircle } from 'react-icons/go'
 import ListItem from '../ListItem/ListItem'
 
 const Lists = () => {
+  const [data, setData] = useState([])
+  const [erro, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/api/listas/@eduarda?format=json')
+      .then(response => {
+        console.log('Raw Response:', response.data)
+        setData(response.data)
+        setLoading(false)
+      })
+      .catch(error => {
+        setError(error)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return <div>Carregando ...</div>
+  if (erro) return <div>Erro ao carregar os dados: {erro.message}</div>
+
   return (
     <div className="ListContent">
       <div className="info">
@@ -12,18 +35,15 @@ const Lists = () => {
         <GoPlusCircle id="add-icon" size={28} />
       </div>
       <div className="lists">
-        <ListItem
-          listName={'Livros 2025'}
-          numBooks={'10'}
-          description={'Livros para ler até o final do ano de 2025'}
-          books={['Livro 1', 'Livro 2']}
-        />
-        <ListItem
-          listName={'Romance'}
-          numBooks={'5'}
-          description={'Livros de romance que todos deveriam ler'}
-          books={['Livro 3', 'Livro 4', 'Livro 5']}
-        />
+        {data.map((list, index) => (
+          <ListItem
+            key={index}
+            listName={list.lista}
+            numBooks={list.livros.length}
+            description={'Sem descrição disponível'}
+            books={list.livros}
+          />
+        ))}
       </div>
     </div>
   )

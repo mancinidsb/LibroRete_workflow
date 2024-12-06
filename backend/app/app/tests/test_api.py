2f8e8272153b69fb7bcbeb7407ae88311b83ae4e
mancinidsb <guilhermemancinidsb@gmail.com>
2024-12-06 07:56:42 -0300

    add test wrong

--- backend/app/app/tests/test_api.py
+++ backend/app/app/tests/test_api.py
@@ -13,4 +13,4 @@ class UsuarioAPITests(TestCase):
         
         # Validações
         self.assertEqual(response.status_code, 200)  # Verifica se a resposta é 200 OK
-        self.assertEqual(response.json(), {"id": 1, "nome": "maria eduarda","username": "@eduarda","email": "eduarda@gmail.com","senha": "2b869053f31a34090f3a8f14cbc73fb5b9cdde56604379c30a11b9b6f43203a4","foto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw0nQQC1W3yDwpOFLJJTqmirx88ESUttZFLA&s"})  # Substitua pelo JSON esperado
+        self.assertEqual(response.json(), {"id": 2, "nome": "maria eduarda","username": "@eduarda","email": "eduarda@gmail.com","senha": "2b869053f31a34090f3a8f14cbc73fb5b9cdde56604379c30a11b9b6f43203a4","foto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw0nQQC1W3yDwpOFLJJTqmirx88ESUttZFLA&s"})  # Substitua pelo JSON esperado
